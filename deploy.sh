60c0ca00f3c34474dfc6547b6022bf1a53bd85a3
Seaninca.me <shiyuan@Shiyuans-MacBook-Pro.local>
2018-05-18 00:49:55 -0700

    python env

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 rm -rf TwitterMining/
 git clone https://github.com/seangone/TwitterMining.git
-cp ./TwitterMining/Server/Deploy/TwitterMining.war ./tomcat/webapps
 
+cp ./TwitterMining/Server/Deploy/TwitterMining.war ./tomcat/webapps

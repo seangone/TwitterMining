838b712ea227099e21dd8fa624257e8c1aeee3dc
Seaninca.me <shiyuan@Shiyuans-MacBook-Pro.local>
2018-05-18 00:30:46 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TwitterMining
+
+
+[http://54.183.78.242:8080/TwitterMining/](http://54.183.78.242:8080/TwitterMining/)
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+rm -rf TwitterMining/
+git clone https://github.com/seangone/TwitterMining.git
+cp ./TwitterMining/Server/Deploy/TwitterMining.war ./tomcat/webapps
+

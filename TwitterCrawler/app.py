140e23190f2e9a6305677b85e22b8570a98f8727
Seaninca.me <>
2018-08-09 18:27:31 -0700

    update app

--- TwitterCrawler/app.py
+++ TwitterCrawler/app.py
@@ -24,22 +24,82 @@ def config_mongo(mongodb_config):
     return db
 
 lasttimestamp = 0
-def writedb_sentimental_score(db, timestamp, score, last_status_id):
+
+def readdb_topics(db):
+    # connect db and get topics
+    dbres = db['topics'].find().limit(400)
+    topics = {l['_id']: l for l in dbres}
+    newrouter = topicrouter(topics)
+    return newrouter
+
+def writedb_sentimental_score(db, topic, timestamp, score, last_status_id):
     global lasttimestamp
     if timestamp - lasttimestamp < 1:
         return
     collection = db['sentiments']
-    collection.insert({'timestamp': timestamp,
+    collection.insert({'topic': topic,
+                       'timestamp': timestamp,
                        'score': score,
                        'last_status_id': last_status_id})
     lasttimestamp = timestamp
 
+def deletedb_old(db, delta = 86400):
+    collection = db['sentiments']
+    res = collection.remove({'timestamp': {"$lt": time.time() - delta}})
+    return res
+
+def get_status_text(status):
+    assert isinstance(status, tweepy.Status)
+    json = status._json
+    if json['text'].startswith("RT"):
+        return
+    if json['truncated']:
+        text = json['extended_tweet']['full_text']
+    else:
+        text = json['text']
+    return text
+
+def get_status_id(status):
+    assert isinstance(status, tweepy.Status)
+    return status._json['id']
+
+class topicrouter(object):
+    def __init__(self, topics = {}):
+        self.topics = topics
+        for k, v in self.topics.items():
+            v['counter'] = windowcounter(600)
+
+    def names(self):
+        return list(self.topics.keys())
+
+    def keywords(self):
+        return [kw for k, v in self.topics.items() for kw in v['keywords']]
+
+    def route(self, status):
+        text = get_status_text(status)
+        if text is None: return
+        for k, v in self.topics.items():
+            for kw in v['keywords']:
+                if kw in text:
+                    return k
+        return
+
+    def get_counter(self, topic_name):
+        if topic_name in self.names():
+            return self.topics[topic_name]['counter']
+        return
+
+    def __eq__(self, another):
+        if not isinstance(another, topicrouter): return False
+        return self.names() == another.names()
+
 
 class MyStreamListener(tweepy.StreamListener):
-    def __init__(self, db, counter):
+    def __init__(self, db, router):
         super(MyStreamListener, self).__init__()
         self.db = db
-        self.counter = counter
+        self.router = router
+        assert isinstance(self.router, topicrouter)
 
     def on_error(self, status_code):
         if status_code == 420:
@@ -48,38 +108,57 @@ class MyStreamListener(tweepy.StreamListener):
             # returning non-False reconnects the stream, with backoff.
             # return True
 
-
     def on_status(self, status):
-        json = status._json
-        if json['text'].startswith("RT"):
-            return
-        if json['truncated']:
-            text = json['extended_tweet']['full_text']
-        else:
-            text = json['text']
-        # Store status info to MongoDB
-        ####
+        # route status to the corresponding topic
+        topic_name = self.router.route(status)
+        if topic_name is None: return
+        counter = self.router.get_counter(topic_name)
+        # TODO: Store status info to MongoDB
         # Calculate Sentimental score
+        id = get_status_id(status)
+        text = get_status_text(status)
         score = cal_sentimental_score(text)
-        self.counter.add(score)
-        print(json['id'], "-----", score, text)
-        print(self.counter(), len(self.counter))
+        counter.add(score)
+        print(status._json)
+        print(id, "-----", score, text)
+        print(topic_name, counter(), len(counter))
         # Store score to MongoDB
-        writedb_sentimental_score(self.db, time.time(), self.counter(), json['id'])
-        # db.sentiments.find().sort({$natural:-1}).limit(1)
+        writedb_sentimental_score(self.db, topic_name, time.time(), counter(), id)
+        # db.sentiments.find({"topic": "Donald Trump"}).sort({"timestamp": -1}).limit(1)
 
 
 if __name__ == '__main__':
-    from util.config import twitter_api_config, mongodb_config
+    from util.config import *
     api = config_tweepy(twitter_api_config)
     db = config_mongo(mongodb_config)
 
-    # connect db and get keywords
+    num_deleted = deletedb_old(db, RESIST_TIME)
+    logger.info("deleted old scores: {}".format(num_deleted))
 
-
-    # start a twitter stream
-    counter = windowcounter(600)
-    myStreamListener = MyStreamListener(db, counter)
+    # get topics from db and start a twitter stream
+    router = readdb_topics(db)
+    myStreamListener = MyStreamListener(db, router)
     myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-    myStream.filter(track=['Donald Trump'])
-    # myStream.filter(follow=["2211149702"])
+    myStream.filter(track=router.keywords(), async=True)
+    logger.info("Topics initialized from DB: {}, keywords: {}"
+                .format(router.names(), router.keywords()))
+
+    while 1:
+        time.sleep(CHECK_INTERVAL)
+        # delete old
+        num_deleted = deletedb_old(db, RESIST_TIME)
+        logger.info("deleted old scores: {}".format(num_deleted))
+        #
+        newrouter = readdb_topics(db)
+        if router is not None and newrouter == router:
+            logger.info("Topics check, nothing changes, keywords: {}"
+                        .format(router.keywords()))
+        else:
+            router = newrouter
+            logger.info("Topics update from DB: {},  keywords: {}"
+                        .format(router.names(), router.keywords()))
+            myStream.disconnect()
+            myStreamListener = MyStreamListener(db, router)
+            myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+            myStream.filter(track=router.keywords(), async=True)
+            logger.info("Stream restarts")

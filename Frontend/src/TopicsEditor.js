726f59fe1858e59b345bcee472463b4e05ba630e
Seaninca.me <>
2018-10-28 03:04:09 -0700

    topic editor update

--- Frontend/src/TopicsEditor.js
+++ Frontend/src/TopicsEditor.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+
+const apiurl = "http://localhost:8080/api";
 const emptytopic = {
     'name': '',
     'keywords': '',
     'userids': ''
-}
+};
 const data = [{
     'name': 'trump',
     'keywords': 'aa',
@@ -13,31 +15,147 @@ const data = [{
     'name': 'james',
     'keywords': '#nba,lebrown',
     'userids': '123,123'
-}]
+}];
 
 class TopicsEditor extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
-        this.state.filterText = "";
+        this.state = {
+            filterText: "",
+            error: null,
+            isLoaded: false,
+            data: data,
+            dataStatus: Array(data.length).fill("")
+        };
     }
+    componentDidMount(){
+        this.fetchTopics();
+    }
+    // communication with back-end
+    fetchTopics() {
+        const url = apiurl + "/topics";
+        console.log("Fetch URL: " + url);
+        fetch(url, {method: 'GET', mode: 'cors'})
+            .then(res => res.json())
+            .then(
+                (raw) => {
+                    console.log("fetchTopics Success: size " + raw.length);
+                    let fined = raw.map((line) => {
+                        return {
+                            name: line._id,
+                            keywords: line.keywords.join(','),
+                            userids: line.ids.join(',')
+                        };
+                    }).filter(line => line.name !== "");
+                    this.setState({
+                        isLoaded: true,
+                        data: fined,
+                        dataStatus: Array(data.length).fill("")
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    console.log("err"+error.message);
+                    this.setState({
+                        isLoaded: true,
+                        error,
+                        data: [],
+                        dataStatus: Array([].length).fill("")
+                    });
+                }
+            )
+    };
+    submitOneToServer(method, index) {
+        var data = this.state.data[index];
+        if (data === undefined || data.name === undefined || data.name === "") {
+            return false;
+        }
+        const url = apiurl + "/topics/";
+        const body = JSON.stringify({
+            "_id": data.name,
+            "keywords": data.keywords.split(",").filter((item)=>item!==""),
+            "ids": data.userids.split(",").filter((item)=>item!=="")
+        });
+        console.log(method + " URL: " + url);
+        console.log(body);
+        if (method === "submit") {
+            fetch(url, {
+                method: 'POST',
+                mode: 'cors',
+                body: body,
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .catch(error => {
+                console.log('--Error:', error);
+                var newDataStatus = this.state.dataStatus.slice();
+                newDataStatus[index] = "error";
+                this.setState({dataStatus: newDataStatus});
+            })
+            .then(response => {
+                console.log('--Success:', response);
+                var newDataStatus = this.state.dataStatus.slice();
+                newDataStatus[index] = "submitted";
+                this.setState({dataStatus: newDataStatus});
+            });
+        } else if (method === "delete") {
+            fetch(url + data.name, {
+                method: 'DELETE',
+                mode: 'cors',
+                body: body,
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .catch(error => {
+                console.log('Error:', error);
+                var newDataStatus = this.state.dataStatus.slice();
+                newDataStatus[index] = "error";
+                this.setState({dataStatus: newDataStatus});
+            })
+            .then(response => {
+                console.log('--Deleted:', response);
+                this.state.data.splice(index, 1);
+                this.state.dataStatus.splice(index, 1);
+                this.setState(this.state.data);
+                this.setState(this.state.dataStatus);
+            })
+        }
+
+        return ;
+    };
+    // handle submitting events
+    handleSubmit(index) {
+        if (index === -1) {
+            for(let ix = 0; ix < this.state.data.length; ix++) {
+                this.submitOneToServer("submit", ix);
+            }
+        } else {
+            this.submitOneToServer("submit", index);
+        }
+        return true;
+    };
     // handle all filtering events
     handleUserInput(filterText) {
         this.setState({filterText: filterText});
     };
     // handle all editing events
     handleRowDel(index) {
-        // var index = this.props.data.indexOf(topic);
-        this.props.data.splice(index, 1);
-        this.setState(this.props.data);
-        this.props.submitToServer("delete", this.props.data);
+        this.submitOneToServer("delete", index);
     };
     handleAddEvent(evt) {
         // var id = (+ new Date() + Math.floor(Math.random() * 999999)).toString(36);
         var topic = Object.assign({}, emptytopic);
         // topic.name = id;
-        this.props.data.push(topic);
-        this.setState(this.props.data);
+        this.state.data.push(topic);
+        this.state.dataStatus.push("");
+        this.setState(this.state.data);
+        this.setState(this.state.dataStatus);
 
     };
     handleTableUpdate(evt) {
@@ -48,7 +166,7 @@ class TopicsEditor extends React.Component {
         };
 
         console.log("Update:" + evt.target.id);
-        var data = this.props.data.slice();
+        var data = this.state.data.slice();
         // var newData = data.map((topic, index) => { // O(n)
         //     if (topic.name == item.id) {
         //         topic[item.name] = item.value;
@@ -56,19 +174,22 @@ class TopicsEditor extends React.Component {
         //     return topic;
         // });
         data[item.id][item.name] = item.value;
+        var dataStatus = this.state.dataStatus.slice();
+        dataStatus[item.id] = "changed";
         this.setState({data:data});
-        //  console.log(this.props.data);
-    };
-    // handle submitting events
-    handleSubmit(index) {
-        if (index === -1) {
-            this.props.submitToServer("submit all", this.props.data);
-        } else {
-            this.props.submitToServer("submit", this.props.data[index]);
-        }
+        this.setState({dataStatus:dataStatus});
+        //  console.log(this.state.data);
     };
     //rendering
     render() {
+        const { error, isLoaded} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else if (this.state.data == null) {
+            return <div>Null</div>;
+        }
 
         return (
             <div>
@@ -78,7 +199,8 @@ class TopicsEditor extends React.Component {
                               onSubmit={this.handleSubmit.bind(this)}
                               onRowAdd={this.handleAddEvent.bind(this)}
                               onRowDel={this.handleRowDel.bind(this)}
-                              data={this.props.data}
+                              data={this.state.data}
+                              dataStatus={this.state.dataStatus}
                               filterText={this.state.filterText}/>
             </div>
         );
@@ -115,18 +237,18 @@ class ProductTable extends React.Component {
         var onRowDel = this.props.onRowDel;
         var onSubmit = this.props.onSubmit;
         var filterText = this.props.filterText;
-        var rows = this.props.data.map( (topic, index)=> {
-            if (topic.name.indexOf(filterText) === -1) {
-                return ;
-            }
-            return (
-                <ProductRow key={index}
-                            index={index}
-                            topic={topic}
-                            onTableUpdate={onTableUpdate}
-                            onSubmit={(e)=>onSubmit(index)}
-                            onRowDel={(e)=>onRowDel(index)}
-                            />)
+        var rows = this.props.data
+            .filter(topic => topic.name.indexOf(filterText) !== -1)
+            .map( (topic, index)=> {
+                return (
+                    <ProductRow key={index}
+                                index={index}
+                                topic={topic}
+                                dataStatus={this.props.dataStatus[index]}
+                                onTableUpdate={onTableUpdate}
+                                onSubmit={(e)=>onSubmit(index)}
+                                onRowDel={(e)=>onRowDel(index)}
+                    />)
         });
         return (
             <div>
@@ -140,15 +262,15 @@ class ProductTable extends React.Component {
                         className="btn btn-outline-secondary float-right">
                     Add Topic
                 </button>
-                <table className="table table-bordered">
+                <table className="table table-hover">
                     <thead>
                     <tr>
-                        <th>Index</th>
-                        <th>Name</th>
-                        <th>Keywords</th>
-                        <th>UserIds</th>
-                        <th>Submit</th>
-                        <th>Delete</th>
+                        <th>#</th>
+                        <th style={{width: "20%"}}>Name</th>
+                        <th style={{width: "33%"}}>Keywords</th>
+                        <th style={{width: "33%"}}>UserIds</th>
+                        <th>✓</th>
+                        <th>✗</th>
                     </tr>
                     </thead>
 
@@ -167,8 +289,16 @@ class ProductTable extends React.Component {
 
 class ProductRow extends React.Component {
     render() {
+        let trstyle = "";
+        if (this.props.dataStatus ===undefined){
+
+        } else if (this.props.dataStatus === "changed") {
+            trstyle = "table-warning"
+        } else if (this.props.dataStatus === "error") {
+            trstyle = "table-danger"
+        }
         return (
-            <tr className="eachRow">
+            <tr className={"eachRow " + trstyle}>
                 <td>{this.props.index}</td>
                 <EditableCell onTableUpdate={this.props.onTableUpdate}
                               cellData={{
@@ -194,13 +324,13 @@ class ProductRow extends React.Component {
                 <td className="submit-cell">
                     <input type="button"
                            onClick={this.props.onSubmit}
-                           value="Submit"
+                           value="✓"
                            className="submit-btn btn-outline-primary"/>
                 </td>
                 <td className="del-cell">
                     <input type="button"
                            onClick={this.props.onRowDel}
-                           value="Delete"
+                           value="✗"
                            className="del-btn btn-outline-danger"/>
                 </td>
             </tr>
